67029e13dd2acb5bc404d3f6ac37711bf1d48bd4
Peter Kröner <peter@peterkroener.de>
2021-01-28 15:01:32 +0100

    make jumping to anchors work in the demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,4 +23,9 @@ module.exports = {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/no-explicit-any": "off",
   },
+
+  env: {
+    browser: true,
+    node: true,
+  }
 };
--- demo/staticsite/script.js
+++ demo/staticsite/script.js
@@ -1,16 +1,42 @@
 const importElement = document.querySelector("html-import");
+const loaderElement = document.querySelector(".loading");
 
 const navLinks = Object.fromEntries(
   Array.from(document.querySelectorAll("nav a"), (a) => [a.href, a])
 );
 let activeNavLink = document.querySelector("nav a.active");
 
+function scrollTo(hash) {
+  if (hash) {
+    if (hash === "#top") {
+      window.scrollTo({ top: 0 });
+      return;
+    }
+    const target = document.querySelector(hash);
+    if (target) {
+      target.scrollIntoView();
+    }
+  }
+}
+
 function goTo(href) {
-  activeNavLink.classList.remove("active");
-  activeNavLink = navLinks[href];
-  activeNavLink.classList.add("active");
-  importElement.src = href;
-  importElement.done.then(([{ title }]) => (window.document.title = title));
+  const parsed = new URL(href);
+  const hash = parsed.hash;
+  const site = parsed.origin + parsed.pathname;
+  if (site !== importElement.src) {
+    loaderElement.removeAttribute("hidden");
+    activeNavLink.classList.remove("active");
+    activeNavLink = navLinks[site];
+    activeNavLink.classList.add("active");
+    importElement.src = site;
+    importElement.done.then(([{ title }]) => {
+      window.document.title = title;
+      loaderElement.setAttribute("hidden", "hidden");
+      scrollTo(hash);
+    });
+  } else {
+    scrollTo(hash);
+  }
 }
 
 window.addEventListener("popstate", ({ state }) => {
